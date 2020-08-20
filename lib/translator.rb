315a05a2e4b9da3dac3016f574741df1b5f58ebe
Gerard Mistretta <gerardmistretta@yahoo.com>
2020-08-20 19:05:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,18 +2,18 @@
 require 'yaml'
 
 def load_library(file_path)
-  # code goes here
+  #code goes here
   library = {"get_meaning" => {}, "get_emoticon" => {} }
-   YAML.load_file(file_path).each do |meaning, array|
-     english, japanese = array
-     library["get_emoticon"][english] = japanese
-     library["get_meaning"][japanese] = meaning
-   end
-   library
- end
+  YAML.load_file(file_path).each do |meaning, array|
+    english, japanese = array
+    library["get_emoticon"][english] = japanese
+    library["get_meaning"][japanese] = meaning
+  end
+  library
+end
 
 def get_japanese_emoticon(file_path, emoticon)
-  # code goes here
+  #code goes here
   library = load_library(file_path)
   result = library["get_emoticon"][emoticon]
   if result
@@ -24,7 +24,7 @@ def get_japanese_emoticon(file_path, emoticon)
 end
 
 def get_english_meaning(file_path, emoticon)
-  # code goes here
+  #code goes here
   library = load_library(file_path)
   result = library["get_meaning"][emoticon]
   if result
